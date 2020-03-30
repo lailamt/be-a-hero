f72ed71b58ef2a7dfa2077272a3140e5203bd1cb
lailamt <lailapmota@gmail.com>
2020-03-29 22:18:15 -0300

    Front-end e conexão com api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css'
+
+import Routes from './routes';
+import { Route } from 'react-router-dom';
+
+// import Header from './Header'; //Não estamos mais utilizando esse arquivo, pois tudo já está aqui
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Omnistack.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
